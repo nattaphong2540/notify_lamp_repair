c3f3bf82538903295a302b7e37a31e8dd13717af
nattaphong2540 <nattaphong.p@kkumail.com>
2019-12-13 23:08:39 +0700

    create Users table in database, add User Page and function [add, update, delete]

---
+++ application/models/User_model.php
@@ -0,0 +1,55 @@
+<?php
+class User_model extends CI_Model
+{
+    public function get_all_user()
+    {
+
+        $query = $this->db->get_where('users', 'delete_status != "delete"');
+
+        return $query->result();
+    }
+
+    public function insert_user($Username, $Password, $Email, $Status)
+    {
+        $data = array(
+            'username' => $Username,
+            'password' => $Password,
+            'email' => $Email,
+            'status' => $Status
+        );
+
+        $this->db->insert('users', $data);
+
+        return true;
+    }
+
+    public function get_id_user($Uid)
+    {
+        $query = $this->db->get_where('users', array('uid' => $Uid));
+        return $query->row_array();
+    }
+
+    public function update_user($Uid, $Username, $Password, $Email, $Status)
+    {
+        $data = array(
+            'username' => $Username,
+            'password' => $Password,
+            'email' => $Email,
+            'status' => $Status
+        );
+        $this->db->where('uid', $Uid);
+        $this->db->update('users', $data);
+
+        return true;
+    }
+
+    public function delete_user($Uid)
+    {
+        $data = array(
+            'uid' => $Uid,
+            'delete_status' => 'delete'
+        );
+        $this->db->where('uid', $Uid);
+        $this->db->update('users', $data);
+    }
+}

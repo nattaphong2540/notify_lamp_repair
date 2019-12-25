46c12300b0421f0edd4bafdd1ec9eea598fe583a
nattaphong2540 <nattaphong.p@kkumail.com>
2019-12-25 17:45:33 +0700

    commit

--- application/views/login/show_login.php
+++ application/views/login/show_login.php
@@ -5,11 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<<<<<<< HEAD
     <title>Login2</title>
-=======
-    <title>Login</title>
->>>>>>> parent of 0e3b51d... Initial commit
     <!-- plugins:css -->
     <link rel="stylesheet" href="<?= base_url('plugins/serein/') ?>vendors/iconfonts/mdi/font/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="<?= base_url('plugins/serein/') ?>vendors/css/vendor.bundle.base.css">
@@ -105,7 +101,7 @@
             },
         })
     });
-<<<<<<< HEAD
+
 
     // function signInButton() {
     //     var Username = $("#InputUsername").val();
@@ -122,6 +118,4 @@
     //             userDetail = JSON.parse(resultData);
     //         }
     //     })
-=======
->>>>>>> parent of 0e3b51d... Initial commit
 </script>
